d76b9cafbf88577f04d2c8620ab20f684cb6a42e
Emilwikk <emilwikk@gmail.com>
2025-10-24 13:49:53 +0200

    progress

--- src/train.py
+++ src/train.py
@@ -4,9 +4,16 @@ import time
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 import os
 import numpy as np
 import joblib
+
+def hyper_parameter_tuning(learning_rate_min,learning_rate_max,max_depth_max,max_depth_min,):
+    grid = {
+    "max_depth" : range(max_depth_min,max_depth_max),
+    "learning_rate" : [1,2,3,4]
+    }
 
 # Hyperparameters
 learning_rate = 0.1
@@ -33,7 +40,7 @@ pos = (y_train==1).sum()
 neg = (y_train==0).sum()
 spw = pos/neg
 
-LGBM_model = make_pipeline(StandardScaler(),  
+LGBM_model = make_pipeline(StandardScaler(), 
            LGBMClassifier(n_estimators=n_estimators, random_state=54,max_depth=max_depth,learning_rate=learning_rate,reg_alpha=reg_alpha,reg_lambda=reg_lambda,
                           n_jobs=-1, num_leaves=num_leaves, scale_pos_weight=spw))
 start_time_train = time.time()          # start the timer for training
@@ -42,3 +49,10 @@ end_time_train = time.time()
 joblib.dump(LGBM_model, "model.pkl")
 
 print(f"Training time: {round(end_time_train-start_time_train,3)} s")
+
+grid = {
+    "max_depth" : [2,4,6,8],
+    "learning_rate" : [1,2,3,4]
+}
+
+print(grid)
